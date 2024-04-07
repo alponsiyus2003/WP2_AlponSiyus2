36ede77606f93feb6f7078028ce8a02185fbb336
alponsiyus2003 <alponsiyus281103@gmail.com>
2024-04-07 21:02:04 +0700

    Pertemuan 03

---
+++ application/controllers/home.php
@@ -0,0 +1,7 @@
+<?php
+
+print("Nama : Alpon Siyus<br>");  // Menampilkan Nama Saya
+print("Kelas : 15.4C.30 <br>"); //Menampilkan Kelas Saya
+print("Nim : 15220580 <br>"); //Menampilkan Nomor Induk Mahasiswa Saya
+
+?>
