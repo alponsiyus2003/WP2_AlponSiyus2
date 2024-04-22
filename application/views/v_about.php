d5b5de017b93839ccfdd59738e794d2351fda572
alponsiyus2003 <alponsiyus281103@gmail.com>
2024-04-22 16:13:28 +0700

    pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+        <ul type="disc">
+            <li>Nama Depan : Alpon</li>
+            <li>Nama Belakang : Siyus</li>
+        </ul><br>
+
+    <h4>Alamat</h4>
+        <ul type="none">
+            <li> gang Keluarga, Jl. Parit Hasasusin 1 (Paris 1), 
+                Pontianak Tenggara, Kalimantan Barat, Indonesia</li>
+        </ul>
+ 
+    <h4>Tempat Lahir</h4>
+        <ul type="none">
+            <li>Kec. Nanga pinoh, Kab. Melawi, Kalimantan Barat, Indonesia</li>
+        </ul>
+
+    <h4>Hobi</h4>
+        <ul type="square">
+            <li>Menyanyi</li>
+            <li>Membuat musik/Composser Lagu</li>
+            <li>Traveling</li>
+        </ul>
+</section
